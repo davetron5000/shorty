0b7b9b157f1bfd685ad451dcc5144fce3718b81f
Dave Copeland <davetron5000@gmail.com>
2009-10-18 15:17:49 -0400

    updated version of scalatest plus bdb

--- project/build/Shorty.scala
+++ project/build/Shorty.scala
@@ -4,11 +4,11 @@ class Shorty(info: ProjectInfo) extends DefaultWebProject(info) {
 
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided->default"
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test->default"
-  val scalatest = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "test->default"
+  val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test->default"
   val jwebunit = "net.sourceforge.jwebunit" % "jwebunit-htmlunit-plugin" % "1.5" % "test->default"
   val log4j = "log4j" % "log4j" % "1.2.13" % "provided->default" 
   val easymockclass = "easymock" % "easymockclassextension" % "2.2" % "test->default"
-
+  val berkeleydb = "berkeleydb" % "berkeleydb" % "1.5.1"
   override def jettyContextPath = "/s"
 
   override def defaultTestTask(testOptions: => Seq[TestOption]) =
