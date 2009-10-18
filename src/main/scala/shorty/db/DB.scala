e0a457bf910a301bb7ad2398c62e3f44091abfb8
Dave Copeland <davetron5000@gmail.com>
2009-10-18 17:31:14 -0400

    consolidated everything

--- src/main/scala/shorty/db/DB.scala
+++ src/main/scala/shorty/db/DB.scala
@@ -4,25 +4,7 @@ import java.io._
 
 import com.sleepycat.je._
 
-/** The lowest-level interface to the key/value store.
-  * This is not thread-safe.
-  */
-trait DB {
-  def size:Long = keyValue.size
-  def get(key:String):Option[String] = keyValue.get(key)
-  def put(key:String,value:String):Unit = {
-    keyValue = keyValue + ((key,value))
-  }
-  def close = {}
-  protected var keyValue:Map[String,String]
-}
-
-/** 
-  * An implementation of DB using BerkelyDB
-  */
-class BDB(env:Environment) extends DB with Logs {
-  protected var keyValue:Map[String,String] = Map()
-
+class DB(env:Environment) extends Logs {
   val database = {
     val config = new DatabaseConfig
     config.setAllowCreate(true)
@@ -31,8 +13,8 @@ class BDB(env:Environment) extends DB with Logs {
 
   private implicit def stringToDatabaseEntry(s:String) = new DatabaseEntry(s.getBytes())
 
-  override def size = database.count()
-  override def get(key:String) = {
+  def size = database.count()
+  def get(key:String) = {
     var value:DatabaseEntry = new DatabaseEntry
     database.get(null,key,value,null) match {
       case OperationStatus.SUCCESS => Some(new String(value.getData))
@@ -43,19 +25,21 @@ class BDB(env:Environment) extends DB with Logs {
     }
   }
 
-  override def put(key:String, value:String) = {
+  def put(key:String, value:String) = {
     database.put(null,key,value)
   }
 
-  override def close = {
+  def close = {
     database.close()
     env.close()
-    super.close
   }
+
+  override def finalize = close
 }
 
-object BDB {
+object DB extends Logs {
   def apply(dir:File) = {
+    debug("Creating new DB in " + dir)
     if (!dir.exists()) dir.mkdirs()
 
     val envConfig:EnvironmentConfig = new EnvironmentConfig();
@@ -63,47 +47,6 @@ object BDB {
     envConfig.setAllowCreate(true);
     
     val environment = new Environment(dir,envConfig)
-    new BDB(environment)
-  }
-}
-/**
-  * A simple file-based key/value store
-  */
-class FileDB(file:File) extends DB with Logs {
-  protected var keyValue:Map[String,String] = initFromFile
-
-  override def put(key:String,value:String):Unit = {
-    super.put(key,value)
-    outputToFile
-  }
-
-  private def initFromFile = {
-    if (file.exists()) {
-      try {
-        debug("Reading in " + file)
-        val is = new ObjectInputStream(new FileInputStream(file))
-        keyValue = is.readObject.asInstanceOf[Map[String,String]]
-        is.close
-      }
-      catch {
-        case eof:EOFException => {
-          debug("EOFException While reading " + file)
-          debug("Assumign empty file")
-          keyValue = Map()
-        }
-        case ex:Exception => throw ex
-      }
-    }
-    else {
-      debug("File not there")
-      keyValue = Map()
-    }
-    keyValue
-  }
-
-  private def outputToFile:Unit = {
-    val os = new ObjectOutputStream(new FileOutputStream(file))
-    os.writeObject(keyValue)
-    os.close
+    new DB(environment)
   }
 }
--- src/test/scala/shorty/db/TestDB.scala
+++ src/test/scala/shorty/db/TestDB.scala
@@ -4,28 +4,62 @@ import java.io._
 
 import shorty._
 
-class TestDB extends BaseDBTest {
+class TestDB extends BaseTest {
+
+  var database:DB = _
+
+  override def beforeEach = {
+    val (file,db) = newDB
+    database = db
+  }
+
+  override def afterEach = { 
+    database.close
+  }
 
   protected def newDB:(File,DB) = {
     val tmpFile = File.createTempFile(getClass.getName,"db")
+    tmpFile.delete
+    tmpFile.mkdirs
     tmpFile.deleteOnExit
     (tmpFile,newDB(tmpFile))
   }
 
-  protected def newDB(file:File) = new FileDB(file)
-  describe("FileDB") {
-    it ("should deal with a non-existent file") {
-      var (tmpFile,diskDB) = newDB
-      diskDB.put("key","value")
-      tmpFile.delete
-      diskDB = newDB(tmpFile)
-      diskDB.size should equal(0)
+  protected def newDB(dir:File) = DB(dir)
 
-      tmpFile.delete
-      diskDB.put("key","value")
-      diskDB.size should equal(1)
+  describe("DB") {
+    it ("should initially be empty") {
+      database.size should equal(0)
+    }
+
+    it ("should return None on get of unknown key") {
+      database.get("nevertobeused") should equal(None)
+    }
+
+    it ("should return a get when it was put") {
+      database.put("somekey","somevalue")
+      database.get("somekey") should equal(Some("somevalue"))
+      database.size should equal(1)
+      database.get("nevertobeused") should equal(None)
+    }
+
+    it ("should allow updating a value") {
+      database.put("somekey","somevalue")
+      database.put("somekey","some other value")
+      database.get("somekey") should equal(Some("some other value"))
+    }
+
+
+    it ("should persist to disk") {
+      var (tmpFile,diskDB) = newDB
+
+      diskDB.put("somekey","somevalue")
+      diskDB.close
+
       diskDB = newDB(tmpFile)
+      diskDB.get("somekey") should equal(Some("somevalue"))
       diskDB.size should equal(1)
+      diskDB.close
     }
   }
 }
