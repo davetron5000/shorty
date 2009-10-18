980c02848d5184d5ab48a43fffcefe8dbe92b8f1
Dave Copeland <davetron5000@gmail.com>
2009-10-18 15:32:21 -0400

    reverting back to 0.9.5 since 1.0 breaks everything
    Very strange to change interfaces so severely between essentially an RC version
    and the real one, but 1.0 doesn't work with SBT without some yak-shaving that
    I have no interest in right now.

--- project/build/Shorty.scala
+++ project/build/Shorty.scala
@@ -4,7 +4,7 @@ class Shorty(info: ProjectInfo) extends DefaultWebProject(info) {
 
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided->default"
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test->default"
-  val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test->default"
+  val scalatest = "org.scalatest" % "scalatest" % "0.9.5" % "test->default"
   val jwebunit = "net.sourceforge.jwebunit" % "jwebunit-htmlunit-plugin" % "1.5" % "test->default"
   val log4j = "log4j" % "log4j" % "1.2.13" % "provided->default" 
   val easymockclass = "easymock" % "easymockclassextension" % "2.2" % "test->default"
