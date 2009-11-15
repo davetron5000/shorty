4629412f662e381bbeb22c81c265406f030b7e9b
Dave Copeland <davetron5000@gmail.com>
2009-11-15 14:56:21 -0500

    scaladoc and some refactoring

--- src/main/scala/shorty/AllUrlsController.scala
+++ src/main/scala/shorty/AllUrlsController.scala
@@ -2,11 +2,19 @@ package shorty
 
 import shorty.db._
 
+/**
+  * Controller for manipulating all the urls as a resource.
+  * Basically just lets you add a new URL and get the hash back
+  */
 class AllUrlsController(hasher:URIHasher) extends Controller {
+
   val WAIT_TIME = 2000
+  val URL_PARAM = "url"
+
   override def post(params:Map[String,String]) = {
-    debug("Post with url == " + params("url"))
-    hasher.!?(WAIT_TIME,HashURI(params("url"))) match {
+    debug("Post with url == " + params(URL_PARAM))
+    // this form returns an Option, and we are expecting an Option inside that
+    hasher.!?(WAIT_TIME,HashURI(params(URL_PARAM))) match {
       case Some(Some(hash:String)) => new Right(hash)
       case Some(None) => new Left((500,"Internal error from database"))
       case None => new Left((500,"Timed out hashing the URL"))
