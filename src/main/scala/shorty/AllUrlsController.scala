10231bb957c0a6c80c490f1d5e2c1f20eb5d7d74
Dave Copeland <davetron5000@gmail.com>
2009-11-15 15:15:48 -0500

    extracted common code into parent

--- src/main/scala/shorty/AllUrlsController.scala
+++ src/main/scala/shorty/AllUrlsController.scala
@@ -8,7 +8,6 @@ import shorty.db._
   */
 class AllUrlsController(hasher:URIHasher) extends Controller {
 
-  val WAIT_TIME = 2000
   val URL_PARAM = "url"
 
   override def post(params:Map[String,String]) = {
@@ -17,7 +16,7 @@ class AllUrlsController(hasher:URIHasher) extends Controller {
     hasher.!?(WAIT_TIME,HashURI(params(URL_PARAM))) match {
       case Some(Some(hash:String)) => new Right(hash)
       case Some(None) => new Left((500,"Internal error from database"))
-      case None => new Left((500,"Timed out hashing the URL"))
+      case None => timeout
     }
   }
 }
--- src/main/scala/shorty/Controller.scala
+++ src/main/scala/shorty/Controller.scala
@@ -1,7 +1,9 @@
 package shorty;
 
 abstract class Controller extends Logs {
-  private val notAllowed = new Left((405,"This method is not allowed"))
+  val WAIT_TIME = 2000
+  protected val notAllowed = new Left((405,"This method is not allowed"))
+  protected val timeout = new Left((408,"Request timed out to database"))
 
   def get(params:Map[String,String]):Either[(Int,String),String] = notAllowed
   def put(params:Map[String,String]):Either[(Int,String),String] = notAllowed
