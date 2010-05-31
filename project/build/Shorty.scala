3449bf1298d6bdd6b5d0bde8a29d763dfe61900c
Dave Copeland <davetron5000@gmail.com>
2010-05-31 11:28:29 -0400

    Upgrading to latest sbt and snap of scalatest

--- project/build/Shorty.scala
+++ project/build/Shorty.scala
@@ -3,10 +3,11 @@ import sbt._
 class Shorty(info: ProjectInfo) extends DefaultWebProject(info) {
 
   val oracleRepository = "Oracle Maven Repository" at "http://download.oracle.com/maven"
+  val scalaSnapshots = "scala-tools.org SNAPSHOT Repository" at "http://scala-tools.org/repo-snapshots/"
 
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided->default"
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test->default"
-  val scalatest = "org.scalatest" % "scalatest" % "0.9.5" % "test->default"
+  val scalatest = "org.scalatest" % "scalatest" % "1.1-SNAPSHOT" % "test->default"
   val jwebunit = "net.sourceforge.jwebunit" % "jwebunit-htmlunit-plugin" % "1.5" % "test->default"
   val log4j = "log4j" % "log4j" % "1.2.13" % "provided->default" 
   val easymockclass = "easymock" % "easymockclassextension" % "2.2" % "test->default"
