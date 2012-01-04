501d9e59a9fb17bbc4bc5dbe886696b18d95d084
Adrian Fritsch <adrianfr@gmail.com>
2012-01-04 12:00:16 -0800

    Made loggers return Unit

--- src/main/scala/shorty/Logs.scala
+++ src/main/scala/shorty/Logs.scala
@@ -21,28 +21,29 @@ trait Logs {
 
   import org.apache.log4j.Level._
 
-  def trace(message: => String) = if (logger.isEnabledFor(TRACE)) logger.trace(message)
-  def trace(message: => String, ex:Throwable) = if (logger.isEnabledFor(TRACE)) logger.trace(message,ex)
+  def trace(message: => Unit) = if (logger.isEnabledFor(TRACE)) logger.trace(message)
+  def trace(message: => Unit, ex:Throwable) = if (logger.isEnabledFor(TRACE)) logger.trace(message,ex)
 
-  def debug(message: => String) = if (logger.isEnabledFor(DEBUG)) logger.debug(message)
-  def debug(message: => String, ex:Throwable) = if (logger.isEnabledFor(DEBUG)) logger.debug(message,ex)
+  def debug(message: => Unit) = if (logger.isEnabledFor(DEBUG)) logger.debug(message)
+  def debug(message: => Unit, ex:Throwable) = if (logger.isEnabledFor(DEBUG)) logger.debug(message,ex)
+
   def debugValue[T](valueName: String, value: => T):T = {
     val result:T = value
     debug(valueName + " == " + result.toString)
     result
   }
 
-  def info(message: => String) = if (logger.isEnabledFor(INFO)) logger.info(message)
-  def info(message: => String, ex:Throwable) = if (logger.isEnabledFor(INFO)) logger.info(message,ex)
+  def info(message: => Unit) = if (logger.isEnabledFor(INFO)) logger.info(message)
+  def info(message: => Unit, ex:Throwable) = if (logger.isEnabledFor(INFO)) logger.info(message,ex)
 
-  def warn(message: => String) = if (logger.isEnabledFor(WARN)) logger.warn(message)
-  def warn(message: => String, ex:Throwable) = if (logger.isEnabledFor(WARN)) logger.warn(message,ex)
+  def warn(message: => Unit) = if (logger.isEnabledFor(WARN)) logger.warn(message)
+  def warn(message: => Unit, ex:Throwable) = if (logger.isEnabledFor(WARN)) logger.warn(message,ex)
 
   def error(ex:Throwable) = if (logger.isEnabledFor(ERROR)) logger.error(ex.toString,ex)
-  def error(message: => String) = if (logger.isEnabledFor(ERROR)) logger.error(message)
-  def error(message: => String, ex:Throwable) = if (logger.isEnabledFor(ERROR)) logger.error(message,ex)
+  def error(message: => Unit) = if (logger.isEnabledFor(ERROR)) logger.error(message)
+  def error(message: => Unit, ex:Throwable) = if (logger.isEnabledFor(ERROR)) logger.error(message,ex)
 
   def fatal(ex:Throwable) = if (logger.isEnabledFor(FATAL)) logger.fatal(ex.toString,ex)
-  def fatal(message: => String) = if (logger.isEnabledFor(FATAL)) logger.fatal(message)
-  def fatal(message: => String, ex:Throwable) = if (logger.isEnabledFor(FATAL)) logger.fatal(message,ex)
+  def fatal(message: => Unit) = if (logger.isEnabledFor(FATAL)) logger.fatal(message)
+  def fatal(message: => Unit, ex:Throwable) = if (logger.isEnabledFor(FATAL)) logger.fatal(message,ex)
 }
